fc7597e9dac249b13a18a408737c67aef0fbaf72
shijialong <shijialong8@sina.com>
2016-11-01 16:13:52 +0800

    创建tabBar中间的按钮

---
+++ 百思不得姐/百思不得姐/Classes/JLViewController.h
@@ -0,0 +1,13 @@
+//
+//  JLViewController.h
+//  百思不得姐
+//
+//  Created by shijialong on 2016/10/31.
+//  Copyright © 2016年 shijialong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLViewController : UIViewController
+
+@end
---
+++ 百思不得姐/百思不得姐/Classes/JLViewController.m
@@ -0,0 +1,23 @@
+//
+//  JLViewController.m
+//  百思不得姐
+//
+//  Created by shijialong on 2016/10/31.
+//  Copyright © 2016年 shijialong. All rights reserved.
+//
+
+#import "JLViewController.h"
+
+@interface JLViewController ()
+
+@end
+
+@implementation JLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = JLRandomColor;
+}
+
+
+@end
